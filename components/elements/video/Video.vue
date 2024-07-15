e5b0a84831c99595446415f15ebd9420e9ecba8b
Michał Sitkowski <emsitkowski@gmail.com>
2024-07-15 12:23:16 +0200

    feat: adjust video mask + add global class for styling

--- components/elements/video/Video.vue
+++ components/elements/video/Video.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="relative h-60 lg:h-80 rounded-3xl md:rounded-4xl overflow-hidden">
-    <video class="absolute top-0 left-0 size-full object-cover" autoplay muted loop playsinline>
-      <source src="@/assets/video/Abstract_objects_(dark_theme).mp4" type="video/mp4" />
-    </video>
+  <div class="rounded-3xl md:rounded-4xl select-none pointer-events-none overflow-hidden">
+    <div class="min-h-60 lg:min-h-80 clip-path">
+      <video class="fixed top-0 left-0 size-full object-cover -z-50" autoplay muted loop playsinline>
+        <source src="@/assets/video/Abstract_objects_(dark_theme).mp4" type="video/mp4" />
+      </video>
+
+      <!-- content -->
+      <slot></slot>
+    </div>
   </div>
 </template>
