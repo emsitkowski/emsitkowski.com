33285e35bc8ed5b808ea26fae9de842a9eecf764
Michał Sitkowski <emsitkowski@gmail.com>
2024-07-18 22:01:22 +0200

    feat: optimize and compress background video

--- components/element/video/Video.vue
+++ components/element/video/Video.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="rounded-3xl md:rounded-4xl select-none pointer-events-none overflow-hidden">
-    <div class="min-h-60 lg:min-h-80 clip-path">
-      <video class="fixed top-0 left-0 size-full object-cover -z-50" autoplay muted loop playsinline>
-        <source src="@/assets/video/Abstract_objects_(dark_theme).mp4" type="video/mp4" />
+  <div class="rounded-3xl md:rounded-4xl overflow-hidden">
+    <div class="flex flex-col justify-center items-center min-h-60 lg:min-h-80 clip-path">
+      <video
+        class="fixed top-0 left-0 size-full object-cover -z-50"
+        poster="@/assets/img/background-video-poster.jpg"
+        preload="none"
+        muted
+        loop
+        playsinline
+        ref="video"
+      >
+        <source src="@/assets/video/Abstract_objects_(dark_theme)_compressed.mp4" type="video/mp4" />
       </video>
 
       <!-- content -->
@@ -10,3 +18,11 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const video = ref<HTMLVideoElement>();
+
+onMounted(() => {
+  video.value?.play();
+});
+</script>
