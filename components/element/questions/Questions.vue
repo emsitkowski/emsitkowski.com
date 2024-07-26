d1dc009d5d2f3632446fbc792642c3efe57ea443
Michał Sitkowski <emsitkowski@gmail.com>
2024-07-26 12:33:26 +0200

    feat: adjust questions text

--- components/element/questions/Questions.vue
+++ components/element/questions/Questions.vue
@@ -19,6 +19,11 @@ const questions: QuestionsItem[] = [
     value: `I specialize in a variety of modern web technologies, including: HTML5, CSS3, JavaScript, TypeScript, Vue, Nuxt, Tailwind CSS, Sass, REST APIs, Headless CMS, animation libraries like GSAP or anime.js, Figma, UI component libraries like shadcn or PrimeVue 4, Git, Firebase, Supabase, Vercel, Netlify, and&nbsp;more. <br /><br /> This tech stack allows me to create modern, efficient, and scalable web solutions tailored to your specific&nbsp;needs.`,
   },
   {
+    name: "What are your rates and payment options?",
+    value:
+      "I offer both hourly rates and fixed-price quotes, depending on the project scope. My default hourly rate is $25, but the price may vary depending on the project's complexity. We can also arrange a one-time payment deal, where you pay a fixed, pre-agreed price. No matter which option you choose, each project I work on is tracked with time tracking software like Clockify or Toggl, and you will receive a summary report at the end of the project.<br /><br />For invoicing and payments, I use the Ruul platform, which supports various payment methods and ensures smooth transactions for both domestic and international clients. If you're not comfortable with this option, you can also find me on freelance platforms like Upwork and Contra, which provide secure payments and collaboration tools. The relevant links are at the bottom of this page.",
+  },
+  {
     name: "How long does it take to complete a&nbsp;project?",
     value:
       "It depends on the project itself, but most projects take 1-4 weeks to complete. Reach out to me to discuss the&nbsp;details.",
@@ -53,11 +58,6 @@ const questions: QuestionsItem[] = [
       "While I'm capable of implementing advanced SEO techniques, by default I only include basic SEO&nbsp;optimization. <br /><br /> If advanced SEO is important for your project, please let me know, and we can discuss including it in the scope of work. I can also help you arrange a meeting with a professional SEO&nbsp;agency.",
   },
   {
-    name: "Do&nbsp;you charge hourly or per&nbsp;project?",
-    value:
-      "I offer both hourly rates and fixed-price quotes, depending on the project scope. <br /><br /> No matter which option you choose, each project I work on is tracked with time tracking software like Clockify or Toggl. You will receive a summary report at the end of the&nbsp;project.",
-  },
-  {
     name: "Can you assist with domain registration and&nbsp;web&nbsp;hosting?",
     value:
       "Yes, I can help you select and set up appropriate domain and hosting services. Usually, I can also provide a special promo code for your selected&nbsp;provider.",
@@ -66,11 +66,6 @@ const questions: QuestionsItem[] = [
     name: "Do you provide training for content management system?",
     value:
       "Of course, I offer training sessions to help you manage and update your website&nbsp;content if CMS is included in the&nbsp;project. ",
-  },
-  {
-    name: "How can I pay for your&nbsp;services?",
-    value:
-      "I use the Ruul platform for invoicing and payments. This secure system allows for various payment methods and ensures smooth transactions for both domestic and international clients. <br /><br /> If you're not comfortable with this option, you can find me on freelance platforms like Upwork and&nbsp;Contra, which provide secure payments and collaboration&nbsp;tools. The relevant links are at the bottom of this&nbsp;page.",
   },
 ];
 </script>
